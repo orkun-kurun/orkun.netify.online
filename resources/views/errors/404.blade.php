8399f1edb504523b65592e5e18ef00fef80cd29c
Huseyin Yasin Soysal <huseyinyasin.soysal@campus.netkent.edu.tr>
2022-01-03 20:17:15 +0300

    404 Error Page Desing

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -27,7 +27,7 @@
             <h1 class="font-bold text-blue-600 text-9xl">404</h1>
 
             <h6 class="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
-                <span class="text-red-500">Oops!</span> Page not found
+                <span class="text-red-500">Hoppala!</span> Page not found
             </h6>
 
             <p class="mb-8 text-center text-gray-500 md:text-lg">
